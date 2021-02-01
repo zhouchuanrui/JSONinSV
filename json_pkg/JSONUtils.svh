9d1ae1cee0729297a8488f9463b65edf04fa930f
zhouchuanrui <zhouchuanrui@foxmai.com>
2021-02-02 07:41:28 +0800

    update JSON APIs: 1. add JSONContext 2. add JSONValue APIs 3. add JSONTop

--- json_pkg/JSONUtils.svh
+++ json_pkg/JSONUtils.svh
@@ -8,3 +8,49 @@
 //2021/1/26 7:35:59: created
 //
 
+typedef class JSONValue;
+
+class JSONContext;
+    
+    string json_txt;
+    //string json_buf;
+    int current_idx;
+
+    function new(string json_txt = "NULL");
+        this.json_txt = json_txt;
+        this.current_idx = 0;
+    endfunction
+
+endclass: JSONContext
+
+
+class JSONTop;
+
+    function new();
+    endfunction
+
+    // parser APIs
+    extern JSONStatus loads(string json_txt);
+    extern JSONStatus loadFromFile(string json_file);
+
+    // dumper APIs
+    extern JSONStatus dumps(ref string json_txt);
+    extern JSONStatus dumpToFile(string json_file);
+
+    // internal properties
+    protected int this_depth; // depth of this value node, starts from 0
+    extern JSONStatus parseValue();
+    extern JSONStatus parseWhiteSpace();
+    extern JSONStatus parseObject();
+    extern JSONStatus parseArray();
+    extern JSONStatus parseString();
+    extern JSONStatus parseNumber();
+    extern JSONStatus parseNull();
+    extern JSONStatus parseTrue();
+    extern JSONStatus parseFalse();
+
+endclass: JSONTop
+
+function JSONStatus JSONTop::loads (string json_txt);
+endfunction
+
--- json_pkg/JSONValue.svh
+++ json_pkg/JSONValue.svh
@@ -8,8 +8,50 @@
 //2021/1/26 7:36:46: created
 //
 
+// the JSON value class
 class JSONValue;
-    function new();
+
+    // JSON type enumerate
+    typedef enum {
+        JSON_OBJECT,
+        JSON_ARRAY,
+        JSON_STRING, 
+        JSON_NUMBER, 
+        JSON_TRUE,
+        JSON_FALSE,
+        JSON_NULL
+    } JSONType;
+    JSONType this_type;
+
+    // JSON status enumerate
+    typedef enum {
+        PARSE_OK = 0,
+        PARSE_INVALID_VALUE,
+        PARSE_MISS_QUOTATION_MARK
+    } JSONStatus;
+
+    function new(depth = 0, );
+        this_type = JSON_NULL;
+        this_depth = 0;
     endfunction
-endclass: JSONV
+
+    protected JSONValue this_object[string];
+    protected JSONValue this_array[$];
+    protected string this_string;
+    protected real this_number;
+
+    extern JSONType getType();
+    extern string getTypeString();
+
+    extern void setNull();
+    extern void setTrue();
+    extern void setFalse();
+    extern void setNumber(real number);
+    extern void setString(string str);
+    extern void setObject();
+    extern void addMemberToObject(string key, JSONValue val);
+    extern void setArray();
+    extern void addValueToArray();
+
+endclass: JSONValue
 
